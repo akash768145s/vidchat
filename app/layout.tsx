324759ca8bf7e4d2c97f006d7e1f7e1f145d38dd
akash768145s <akash2210476@ssn.edu.in>
2024-07-22 11:44:38 +0530

    hello

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import NavBar from "@/components/layout/NavBar";
 import Container from "@/components/layout/Container";
+import SocketProvider from "@/providers/SocketProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,12 +22,14 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body className={inter.className}>
+          <SocketProvider>
           <main className="flex min-h-screen flex-col bg-secondary">
             <NavBar />
             <Container>
               {children}
             </Container>
           </main>
+          </SocketProvider>
         </body>
       </html>
     </ClerkProvider>
